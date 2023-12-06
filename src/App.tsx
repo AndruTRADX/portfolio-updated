6d520d018dfc59e025fa386a997f313ee2d3a04f
Andrés Rodríguez <andrudeluxe@gmail.com>
2023-12-06 15:55:16 -0500

    styles changed

--- src/App.tsx
+++ src/App.tsx
@@ -12,13 +12,13 @@ function App() {
       <div className="bg-dark-1 flex flex-col">
         <Home />
 
-        <main className="px-8 relative flex items-center max-w-[1100px] mx-auto">
+        <main className="px-8 relative flex items-center max-w-[1000px] mx-auto">
           <div className="grid grid-cols-2 gap-2 h-full w-full">
             <nav className="flex col-span-1 max-xmd:col-span-2 top-0 h-screen max-xmd:h-auto min-[800px]:sticky gap-3">
               <LeftSideBar />
             </nav>
 
-            <article className="col-span-1 max-xmd:col-span-2 flex flex-col pb-24 px-14 max-xmd:px-2">
+            <article className="col-span-1 max-xmd:col-span-2 flex flex-col pb-24 max-xmd:px-2">
               <About />
               <Projects />
               <Skills />
