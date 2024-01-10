e72ec8e81bff9fd9921f43eb655237e132013df3
Andrés Rodríguez <andrudeluxe@gmail.com>
2024-01-10 14:26:32 -0500

    project added

--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -9,42 +9,56 @@ export interface ProjectDataType {
 export const ProjectsData: ProjectDataType[] = [
   {
     name: "Threads",
-    description: "A full-stack Threads clone crafted with Next.js and Tailwind CSS, allowing users to post their own threads, comment on others, and featuring essential logging functions and much more.",
+    description:
+      "A full-stack Threads clone crafted with Next.js and Tailwind CSS, allowing users to post their own threads, comment on others, and featuring essential logging functions and much more.",
     project: "https://threads-adx.vercel.app/",
     github: "https://github.com/AndruTRADX/threads",
     image: "https://i.ibb.co/2t5TVtX/Screenshot-2023-08-16-170625.png",
   },
   {
+    name: "Airbnb Clone",
+    description:
+      "Experience the power of a full-stack Airbnb clone built with Next.js. This project features OAuth functionalities and CRUD operations, allowing you to seamlessly list your property or make reservations.",
+    project: "https://adxbnb.vercel.app",
+    github: "https://github.com/AndruTRADX/airbnb-clone",
+    image: "https://i.ibb.co/ysw1RzS/image-2024-01-10-142339062.png",
+  },
+  {
     name: "DevMarket",
-    description: "An e-commerce platform designed and developed with React, Tailwind CSS, and Nest.js. It includes logging functionality, CRUD operations, and advanced product search options.",
+    description:
+      "An e-commerce platform designed and developed with React, Tailwind CSS, and Nest.js. It includes logging functionality, CRUD operations, and advanced product search options.",
     project: "https://devmarketadx.vercel.app/",
     github: "https://github.com/AndruTRADX/e-commerce-app",
     image: "https://i.ibb.co/xXYSb4y/image.png",
   },
   {
     name: "Products CRUD",
-    description: "Product CRUD functionality implemented with Next.js and Tailwind CSS, empowering users to effortlessly manage and edit their own products.",
+    description:
+      "Product CRUD functionality implemented with Next.js and Tailwind CSS, empowering users to effortlessly manage and edit their own products.",
     project: "https://fullstack-products-crud.vercel.app/",
     github: "https://github.com/AndruTRADX/fullstack-products-crud",
     image: "https://i.ibb.co/TL7QJ8L/imagen-2023-06-23-105521126.png",
   },
   {
     name: "Metaversus",
-    description: "Join us in an interactive and fun Metaverse experience, complete with captivating animations and a stunning interface.",
+    description:
+      "Join us in an interactive and fun Metaverse experience, complete with captivating animations and a stunning interface.",
     project: "https://metaversus-andrutradx.vercel.app/",
     github: "https://github.com/AndruTRADX/metaversus",
     image: "https://i.ibb.co/SBS1Gff/imagen-2023-06-23-105633430.png",
   },
   {
     name: "Pokedux",
-    description: "Take a look at these creatures on a nice interface which will show you details about your favorite pokemons.",
+    description:
+      "Take a look at these creatures on a nice interface which will show you details about your favorite pokemons.",
     project: "https://andrutradx.github.io/pokedux/",
     github: "https://github.com/AndruTRADX/pokedux",
     image: "https://i.ibb.co/BgYfrz8/imagen-2023-06-23-105742529.png",
   },
   {
     name: "Ts labyrinth",
-    description: "Play a short labyrinth videogame made with canvas and get the best score you can.",
+    description:
+      "Play a short labyrinth videogame made with canvas and get the best score you can.",
     project: "https://andrutradx.github.io/ts-juego-laberinto/",
     github: "https://github.com/AndruTRADX/ts-juego-laberinto",
     image: "https://i.ibb.co/8475G0w/imagen-2023-06-23-105818167.png",
